e00aafdfd30694c07e27c12dd9ae9279358c78c7
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-03-25 23:40:27 +0530

    Add files via upload

--- NSA/add.sh
+++ NSA/add.sh
@@ -1,4 +1,7 @@
-read -p "Enter first number : " a
-read -p "Enter second number : " b
-c=$(($a+$b))
-echo $c
+#!bin/bash
+echo -n "Enter first number : "
+read num1
+echo -n "Enter second number : "
+read num2
+sum=`expr $num1 + $num2`
+echo -n "Sum = " $sum
