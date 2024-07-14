43c195dbcd86be67d6aa8dee2c98c3a83e13b78b
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-07-14 20:01:50 +0530

    Add files via upload

--- NSA/fibo.sh
+++ NSA/fibo.sh
@@ -1,23 +1,13 @@
 #!bin/bash
-echo -n "Enter the limit : "
-read n
+
+read -p "Enter the limit : " n
 a=0
 b=1
-if [ $n -eq 1 ]
-then
-	echo $a
-elif [ $n -eq 2 ]
-then 
-	echo $a
-	echo $b
-else
-	echo $a
-	echo $b
-for((i=2;i<n;i++))
+
+for((i=1;i<=n;i++))
 do
-	c=`expr $a + $b`
-	echo $c
+	echo $a
+	c=$(($a+$b))
 	a=$b
 	b=$c
 done
-fi
