bfcb40f87a13461009f0fceddb7551b7fc6f0163
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-07-18 23:50:38 +0530

    Search Element in Array

--- JAVA/Java/arraySearch.java
+++ JAVA/Java/arraySearch.java
@@ -5,17 +5,13 @@ public class arraySearch {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the length of the array ");
         int n = sc.nextInt();
-
         int arr[] = new int[n];
-
         System.out.println("Enter the elements");
         for(int i=0;i<n;i++)
             arr[i] = sc.nextInt();
-
         System.out.println("Enter the element to search");
         int ele = sc.nextInt();
         int flag=0;
-
         for(int i=0; i<n; i++)
             if(arr[i]==ele){
                 System.out.print(n+ "  is found at the location "+ (i+1));
