50d3d0e43ad7f5ac369b60173ee731cfe7f62018
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-07-18 23:49:54 +0530

    Complex Number Addition

--- JAVA/Java/Complex.java
+++ JAVA/Java/Complex.java
@@ -2,7 +2,6 @@ import java.util.Scanner;;
 
 public class Complex {
     double real, imag;
-
     void getData(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the real part");
@@ -10,22 +9,18 @@ public class Complex {
         System.out.println("Enter the imaginary part");
         imag = sc.nextDouble();
     }
-
     void add(Complex obj1, Complex obj2 ){
         this.real = obj1.real + obj2.real;
         this.imag = obj1.imag + obj2.imag;
         System.out.println("Sum = "+ this.real + " + " + this.imag + "i");
     }
-
     public static void main(String args[]){
         Complex obj1 = new Complex();
         Complex obj2 = new Complex();
-
         obj1.getData();
         obj2.getData();
         obj1.add(obj1,obj2);
-    }
-    
+    }   
 }
 
 
