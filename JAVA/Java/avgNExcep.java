0ef8fabad11ae34e315b0da3d6964232e81896a3
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-07-19 00:17:06 +0530

    Average of N num-Exception

--- JAVA/Java/avgNExcep.java
+++ JAVA/Java/avgNExcep.java
@@ -7,7 +7,6 @@ public class avgNExcep {
             System.out.println("Enter the count of numbers : ");
             int n = sc.nextInt();
             int num[] = new int[n];
-
             System.out.println("Enter the numbers : ");
             for(int i=0; i<n; i++){
                 num[i] = sc.nextInt();
