22fe9ea15970c5d4e183f6257df01e2658329e21
Sreelakshmi Anilkumar <81372148+Sreelakshmi393@users.noreply.github.com>
2024-07-18 23:52:04 +0530

    UserAuth-exception-simple

--- JAVA/Java/authenticUser.java
+++ JAVA/Java/authenticUser.java
@@ -1,23 +1,16 @@
 import java.util.Scanner;
 
-
-
 public class authenticUser {
     public static void main(String[] args) {
-        try{
-            
+        try{  
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter the username : ");
             String username = sc.next();
             sc.nextLine();
-
             if(!username.equals("username")) throw new InvalidUsernameException();
-
             System.out.println("Enter the password : ");
             String password = sc.nextLine();
-            
             if(!password.equals("password")) throw new InvalidPasswordException();
-
             System.out.println("Logged in successfully");
         }catch(InvalidUsernameException e){
             e.printStackTrace();
@@ -28,13 +21,11 @@ public class authenticUser {
         }
     }
 }
-
 class InvalidUsernameException extends RuntimeException{
     InvalidUsernameException(){
         super("Wrong username");
     }
 }
-
 class InvalidPasswordException extends RuntimeException{
     InvalidPasswordException(){
         super("Wrong password");
